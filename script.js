cbbd1f7ef7cfe42ede345d606e36da594bf6ba71
MartinaBasile <martinaleandribasile@gmail.com>
2022-08-25 15:58:05 +0200

    create files html css js with frizzBuzz system based on a loop

---
+++ script.js
@@ -0,0 +1,35 @@
+const grid = document.getElementById("grid")
+
+
+for (let i = 1; i < 101; i++) {
+    const col = document.createElement("div")
+    grid.append(col);
+    col.classList.add("column")
+    let result;
+    if (i % 3 === 0 && i % 5 === 0) {
+        result = "FizzBuzz"
+        col.classList.add("bg-warning")
+    }
+    else if (i % 3 === 0) {
+        result = "Frizz"
+        col.classList.add("bg-primary")
+    }
+    else if (i % 5 === 0) {
+        result = "Buzz"
+        col.classList.add("bg-info")
+    }
+    else {
+        result = i
+        col.classList.add("bg-light")
+
+    }
+    col.innerHTML = result
+    console.log(result)
+
+    col.addEventListener("mouseover", function () {
+        col.classList.add("onevent")
+    })
+    col.addEventListener("mouseout", function () {
+        col.classList.remove("onevent")
+    })
+}
